41a5ac19eb1431267e6ea0551d8f10a0bdb5dccb
Ricardo Garcia Vega <bigardone@gmail.com>
2021-05-26 06:16:10 +0200

    elixir 1.12.0

--- libcluster_cluster/mix.exs
+++ libcluster_cluster/mix.exs
@@ -5,7 +5,7 @@ defmodule LibclusterCluster.MixProject do
     [
       app: :libcluster_cluster,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
